4a12d211e26a574ce8dde89705af4ff788dbea8f
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-11-10 03:36:53 +0000

    Removing mongodb

--- labsadbackend/labsadbackend/repo/ticketRepo.py
+++ labsadbackend/labsadbackend/repo/ticketRepo.py
@@ -1,16 +1,7 @@
 import pymongo
-from pymongo import MongoClient, InsertOne
 from datetime import datetime
 import yfinance as yf
 from datetime import datetime, timedelta
-
-uri = 'mongodb://mongoadmin:a79c987b4dce244e9bc21620@vsgate-s1.dei.isep.ipp.pt:10777'  # MongoDB connection URI
-db_name = 'labsad'       # Replace with your database name
-collection_name = 'stockPrices'  # Replace with your collection name
-
-client = MongoClient(uri)
-db = client[db_name]
-collection = db[collection_name]
 
 class TicketRepo:
     def __init__(self):
